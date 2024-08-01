f8edfd3e78659584f78468553b8e7f1efcd5e6b1
betalantz <betalantz@gmail.com>
2024-08-01 15:31:54 -0700

    completes seed script

--- seeds.py
+++ seeds.py
@@ -45,9 +45,27 @@ def seed():
     # Connecting to Fauna DB
     server_client = FaunaClient(secret=server_secret)
 
+    # Creating the collection
+    event_collection = server_client.query(q.create_collection({"name": "Events"}))
+    print(event_collection)
+
+    # Creating the index
+    events_index = server_client.query(
+        q.create_index(
+            {
+                "name": "Events_by_date",
+                "source": q.collection("Events"),
+                "terms": [{"field": ["data", "date"]}],
+            }
+        )
+    )
+    print(events_index)
+
     # Read the CSV file
     df = pd.read_csv("./paris-olympics-2024-events.csv")
-    # print(df.head(5))
+    print(df.head(5))
+
+    # Cleaning the data
     df["Additional details"] = df["Additional details"].fillna("No additional details")
     df["Location"] = df["Location"].fillna("No location")
     df["Start time"] = df["Start time"].fillna("No start time")
@@ -60,8 +78,42 @@ def seed():
             else x
         )
     )
+    df["End time"] = df["End time"].apply(
+        lambda x: (
+            str(pd.to_datetime(x).time())
+            if pd.to_datetime(x, errors="coerce") is not pd.NaT
+            else x
+        )
+    )
+    df["Date"] = df["Date"].apply(
+        lambda x: (
+            str(pd.to_datetime(x).time())
+            if pd.to_datetime(x, errors="coerce") is not pd.NaT
+            else x
+        )
+    )
+
+    # Loop through each row in the dataframe and create documents
+    for _, row in df.iterrows():
+        result = server_client.query(
+            q.create(
+                q.ref(q.collection("Events"), row.name),
+                {
+                    "data": {
+                        "date": row["Date"],
+                        "sport": row["Sport"],
+                        "event": row["Event"],
+                        "details": row["Additional details"],
+                        "location": row["Location"],
+                        "start": row["Start time"],
+                        "end": row["End time"],
+                    }
+                },
+            )
+        )
+        print(result)
 
 
 if __name__ == "__main__":
-    createDB()
+    # createDB()
     seed()
